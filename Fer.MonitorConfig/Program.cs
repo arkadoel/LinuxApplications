406c37ef99b246dde60c7a5c42e47c17c525b4f6
Fer Minguela <fer.d.minguela@gmail.com>
2025-08-27 15:23:52 +0200

    #1 new monitor config

--- Fer.MonitorConfig/Program.cs
+++ Fer.MonitorConfig/Program.cs
@@ -1,18 +1,30 @@
 ﻿using Avalonia;
+using Fer.MonitorConfig;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace Fer.MonitorConfig;
 
 class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
-    [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    internal const string APP_VERSION = "1.0.1";
+    internal const string APP_NAME = "Monitor Config";
+    internal const string ICONO = "./media/LOGO.png";
 
-    // Avalonia configuration, don't remove; also used by visual designer.
+    internal static ServiceProvider? ServiceProvider;
+
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        ServiceProvider = new ServiceCollection()
+            .AddSingleton<ILogic>(new Logic())
+            .BuildServiceProvider();
+
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+    }
+
+    // Avalonia configuration, don"t remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
