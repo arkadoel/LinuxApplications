ec205d460a7114d845d7dd7aea024a24c51c80a3
Fer Minguela <fer.d.minguela@gmail.com>
2025-08-29 16:37:29 +0200

    #5 Alternativa Selene Media Converter

--- Fer.SeleneBatch/Fer.SeleneBatch/MainWindow.axaml.cs
+++ Fer.SeleneBatch/Fer.SeleneBatch/MainWindow.axaml.cs
@@ -7,5 +7,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        this.Title = $"{Program.APP_NAME} {Program.APP_VERSION}";
     }
 }
--- Fer.SeleneBatch/Fer.SeleneBatch/Program.cs
+++ Fer.SeleneBatch/Fer.SeleneBatch/Program.cs
@@ -1,16 +1,26 @@
 ﻿using Avalonia;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using Fer.SeleneBatch.Core;
 
 namespace Fer.SeleneBatch;
 
 class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
+    internal const string APP_VERSION = "0.1.0";
+    internal const string APP_NAME = "Selene Batch";
+    internal static ServiceProvider? ServiceProvider;
+
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        ServiceProvider = new ServiceCollection()
+            //.AddSingleton<ILogic>(new Logic())
+            .BuildServiceProvider();
+
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
